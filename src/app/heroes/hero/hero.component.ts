fa3ffb8d20300f0e435367e4985d51d03bc80287
Juan Loaiza <sistemas2@tyb.co>
2023-12-20 14:38:25 -0500

    Fin de seccion 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+
+export class HeroComponent {
+
+  public name: string = 'spider-man';
+  public age: number = 45;
+
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+     return `${this.name} - ${this.age}`;
+  }
+
+  changeHero():void{
+      this.name = 'Iron man';
+  }
+
+  changeAge():void{
+      this.age = 25;
+  }
+
+  resetForm():void{
+    this.name = 'spider-man';
+    this.age = 45;
+  }
+
+
+}
